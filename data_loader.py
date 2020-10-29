61f56c723983c795ffcd6c0faabaa9bf44c3cfa7
Divi Schmidt <divi@berkeley.edu>
2020-10-29 12:42:29 -0600

    fixed bug in data loader

--- data_loader.py
+++ data_loader.py
@@ -71,7 +71,7 @@ def load_data(val_split=(32, 8), keypoints=[-6]):
     asf_data = [read_asf(f, keypoints) for f in asf_files]
     asf_data = np.row_stack([format_points(x[0], x[1], width, height) for x in asf_data])
 
-    train_ratio = val_split[0] / val_split[0] + val_split[1]
+    train_ratio = val_split[0] / (val_split[0] + val_split[1])
     train_imgs, val_imgs = np.split(imgs, [int(train_ratio * imgs.shape[0])], axis=0)
     train_asf, val_asf = np.split(asf_data, [int(train_ratio * imgs.shape[0])], axis=0)
 
